7c0974cee474ffccfecbb8ae85cb39832dab8ba9
Dario Gladys <dariogla@va-f1.kzn.21-school.ru>
2024-03-27 21:12:25 +0300

    change directory

--- src/main/java/ru/minsafin/forum/ForumApplication.java
+++ src/main/java/ru/minsafin/forum/ForumApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ForumApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
     }
